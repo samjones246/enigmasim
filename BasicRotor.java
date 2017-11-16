e3c3745717cc8f24e2505260b98604d134fef590
sj2g17 <sam.jones240699@gmail.com>
2017-11-16 14:10:39 +0000

    Part 2

---
+++ BasicRotor.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sj2g17 on 16/11/17.
+ */
+public class BasicRotor extends Rotor {
+    @Override
+    public void initialise(String name) {
+
+    }
+
+    @Override
+    public void substitute(String name) {
+
+    }
+}
---
+++ TurnoverRotor.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sj2g17 on 16/11/17.
+ */
+public class TurnoverRotor extends BasicRotor {
+}
